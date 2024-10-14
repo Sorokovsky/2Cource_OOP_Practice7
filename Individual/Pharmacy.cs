70620356209aaf011ce62fa0ef2799f8de85310d
Sorokovsky <Sorokovskys@ukr.net>
2024-10-14 18:43:06 +0300

    individual

---
+++ Individual/Pharmacy.cs
@@ -0,0 +1,32 @@
+﻿namespace Individual
+{
+    public class Pharmacy
+    {
+        private int _price;
+
+        public string Name { get; set; } = string.Empty;
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value < 0 ? 0 : value; }
+        }
+
+        public Pharmacy(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nPrice: {Price}";
+        }
+
+        public static Pharmacy Enter()
+        {
+            Console.Write("Enter a name: "); string name = Console.ReadLine();
+            Console.Write("Enter a price: "); int price = Convert.ToInt32(Console.ReadLine());
+            return new(name, price);
+        }
+    }
+}
---
+++ Individual/Program.cs
@@ -0,0 +1,67 @@
+﻿//variant 12
+namespace Individual;
+public static class Program
+{
+    public static void Main()
+    {
+        List<Pharmacy> pharmacies = new List<Pharmacy>();
+        while (true)
+        {
+            try
+            {
+                int operation = ChooseOperation();
+                switch (operation)
+                {
+                    case 0:
+                        {
+                            return;
+                        }
+                    case 1:
+                        {
+                            pharmacies.Add(Pharmacy.Enter());
+                            break;
+                        }
+                    case 2:
+                        {
+                            int i = 1;
+                            foreach(var item in pharmacies)
+                            {
+                                Console.WriteLine($"#{i}");
+                                Console.WriteLine(item);
+                                i++;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            var founded = pharmacies.Where(x => x.Price >= 1000);
+                            Console.WriteLine($"Pharmacies that price >= 1000.");
+                            int i = 1;
+                            foreach(var item in founded)
+                            {
+                                Console.WriteLine($"#{i}");
+                                Console.WriteLine(item);
+                            }
+                            break;
+                        }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                continue;
+            }
+        }
+    }
+
+    public static int ChooseOperation()
+    {
+        Console.WriteLine("Choose operation.");
+        Console.WriteLine("0-Exit.");
+        Console.WriteLine("1-Add new pharmacy");
+        Console.WriteLine("2-Show all");
+        Console.WriteLine("3-Show that price >= 1000uah.");
+        Console.Write(">> ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+}
