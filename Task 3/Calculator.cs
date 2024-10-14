e33818e8f684c457a5c13a96b0c3502dde949f50
Sorokovsky <Sorokovskys@ukr.net>
2024-10-14 18:21:24 +0300

    task 3

---
+++ Task 3/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_3
+{
+    public static class Calculator
+    {
+        public static dynamic Add(dynamic a, dynamic b)
+        {
+            return a + b;
+        }
+
+        public static dynamic Subtract(dynamic a, dynamic b)
+        {
+            return a - b;
+        }
+
+        public static dynamic Multiply(dynamic a, dynamic b)
+        {
+            return a * b;
+        }
+
+        public static dynamic Divide(dynamic a, dynamic b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_3;
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"10 + 2 = {Calculator.Add(10, 2)}");
+        Console.WriteLine($"10 - 2 = {Calculator.Subtract(10, 2)}");
+        Console.WriteLine($"10 * 2 = {Calculator.Multiply(10, 2)}");
+        Console.WriteLine($"10 / 2 = {Calculator.Divide(10, 2)}");
+    }
+}
